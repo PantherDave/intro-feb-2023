1caf04da77749530a055a4de6fad58c584683f47
David Acosta <dacostacsit@gmail.com>
2023-02-06 15:07:33 -0500

    Simple status message

---
+++ day1/Finger/Program.cs
@@ -0,0 +1,16 @@
+﻿using Finger;
+
+Console.Write("What is your status: ");
+string status = Console.ReadLine();
+
+// Type identifier = new constructor
+if (status != null)
+{
+    StatusMessage myStatus = new StatusMessage(status, DateTimeOffset.Now);
+    System.Console.WriteLine($"You said your status was {myStatus.Status} at {myStatus.When:T}");
+}
+else
+{
+    Console.WriteLine("Sorry, cannot have a null status");
+}
+
---
+++ day1/Finger/StatusMessage.cs
@@ -0,0 +1,6 @@
+using System;
+
+namespace Finger
+{
+    public record StatusMessage(string Status, DateTimeOffset When);
+}
