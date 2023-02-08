d8a939603dbdbe3357f4a232a842b45779d2219c
David Acosta <dacostacsit@gmail.com>
2023-02-08 15:22:52 -0500

    Pretty currying

--- practices/stringcalculator-wednesday1/StringCalculator.cs
+++ practices/stringcalculator-wednesday1/StringCalculator.cs
@@ -8,7 +8,10 @@ public class StringCalculator
 
     public int Add(string numbers)
     {
-        return numbers.Equals("") ? 0 : Regex.Split(numbers, @"[^\d]")
-            .ToList().Where(s => Double.TryParse(s, out _)).Select(x => int.Parse(x)).Sum();
+        return numbers
+            .Equals("") ? 0 : Regex.Split(numbers, @"[^\d]")
+            .ToList().Where(s => Double.TryParse(s, out _))
+            .Select(x => int.Parse(x))
+            .Sum();
     }
 }
