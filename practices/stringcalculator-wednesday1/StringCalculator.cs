658f977ac5a4b864d3d786d0a5dd42df3fbb3afe
David Acosta <dacostacsit@gmail.com>
2023-02-08 15:27:51 -0500

    Better Formatting

--- practices/stringcalculator-wednesday1/StringCalculator.cs
+++ practices/stringcalculator-wednesday1/StringCalculator.cs
@@ -9,7 +9,8 @@ public class StringCalculator
     public int Add(string numbers)
     {
         return numbers
-            .Equals("") ? 0 : Regex.Split(numbers, @"[^\d]")
+            .Equals("") ? 0 : 
+            Regex.Split(numbers, @"[^\d]")
             .ToList()
             .Where(s => Double.TryParse(s, out _))
             .Select(x => int.Parse(x))
