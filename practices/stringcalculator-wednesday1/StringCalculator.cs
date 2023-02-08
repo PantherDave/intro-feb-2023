64908f0e442a69fbc129764aba4c078695a0b47c
David Acosta <dacostacsit@gmail.com>
2023-02-08 15:14:01 -0500

    Parts 1-4

---
+++ practices/stringcalculator-wednesday1/StringCalculator.cs
@@ -0,0 +1,14 @@
+﻿
+using System.Text.RegularExpressions;
+
+namespace StringCalculator;
+
+public class StringCalculator
+{
+
+    public int Add(string numbers)
+    {
+        return numbers.Equals("") ? 0 : Regex.Split(numbers, @"[^\d]")
+            .ToList().Where(s => Double.TryParse(s, out _)).Select(x => int.Parse(x)).Sum();
+    }
+}
---
+++ practices/stringcalculator-wednesday1/StringCalculatorTests.cs
@@ -0,0 +1,47 @@
+﻿
+namespace StringCalculator;
+
+public class StringCalculatorTests
+{
+    [Fact]
+    public void EmptyStringReturnsZero()
+    {
+        var calculator = new StringCalculator();
+
+        var result = calculator.Add("");
+
+        Assert.Equal(0, result);
+    }
+
+    [Theory]
+    [InlineData("1", 1)]
+    [InlineData("2", 2)]
+    [InlineData("100", 100)]
+    public void SingleDigit(string numbers, int answer)
+    {
+        var calculator = new StringCalculator();
+        var sum = calculator.Add(numbers);
+        Assert.Equal(sum, answer);
+    }
+
+    [Theory]
+    [InlineData("1,2", 3)]
+    [InlineData("3,4,5", 12)]
+    [InlineData("10,20,30,40,50", 150)]
+    public void MultipleDigits(string numbers, int answer)
+    {
+        var calculator = new StringCalculator();
+        var sum = calculator.Add(numbers);
+        Assert.Equal(sum, answer);
+    }
+
+    [Theory]
+    [InlineData("1\n2,3", 6)]
+    [InlineData("//;\n1;2", 3)]
+    public void DifferentDelimiters(string numbers, int answer)
+    {
+        var calculator = new StringCalculator();
+        var sum = calculator.Add(numbers);
+        Assert.Equal(sum, answer);
+    }
+}
