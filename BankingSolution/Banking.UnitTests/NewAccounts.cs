efb5147dcc66eb9eeb0e904dc2219562dee87f57
David Acosta <dacostacsit@gmail.com>
2023-02-08 11:20:02 -0500

    Banking App with passing test

---
+++ BankingSolution/Banking.UnitTests/NewAccounts.cs
@@ -0,0 +1,16 @@
+using Banking.Domain;
+
+namespace Banking.UnitTests;
+
+public class NewAccounts
+{
+    [Fact]
+    public void NewAccoutnsHaveTheCorrewctOpeningBalance()
+    {
+        var account = new BankAccount();
+
+        decimal openingBalance = account.GetBalance();
+
+        Assert.Equal(5_000M, openingBalance);
+    }
+}
