0df2c165ad8b9fd4eb6f04d57dda4939f94024e0
David Acosta <dacostacsit@gmail.com>
2023-02-09 16:06:11 -0500

    Bonus Mock tests

--- BankingSolution/Banking.Domain/BankAccount.cs
+++ BankingSolution/Banking.Domain/BankAccount.cs
@@ -38,4 +38,3 @@ public class BankAccount
         return _balance >= amountToWithdarw;
     }
 }
-
---
+++ BankingSolution/Banking.UnitTests/BonusCalculations/StandardBonusCalculatorTestsDuringBusinessHours.cs
@@ -0,0 +1,27 @@
+﻿namespace Banking.UnitTests.BonusCalculations;
+
+public class StandardBonusCalculatorTestsDuringBusinessHours
+{
+    private StandardBonusCalculator _calculator;
+    public StandardBonusCalculatorTestsDuringBusinessHours()
+    {
+        var stubbedClock = new Mock<IProvideTheBusinessClock>();
+        stubbedClock.Setup(c => c.IsDuringBusinessHours()).Returns(true);
+        _calculator = new StandardBonusCalculator(stubbedClock.Object);
+    }
+    // 1. Deposits under the cutoff amount get no bonus. (5000)
+    [Fact]
+    public void UnderCutoffGetNoBonus()
+    {
+        var bonus = _calculator.GetDepositBonusFor(4999.99M, 100);
+        Assert.Equal(0, bonus);
+    }
+    [Fact]
+    public void AtCutoffGetsBonus()
+    {
+        var bonus = _calculator.GetDepositBonusFor(5000M, 100);
+        Assert.Equal(10, bonus);
+    }
+    // 2. Deposits with 5000+ during Business Hours get a bonus.
+    // 3. Deposits with 5000+ outside of Business Hours get no bonus.
+}
