b3677dd49ed17abb74fc5cddbdb75c003363d538
David Acosta <dacostacsit@gmail.com>
2023-02-09 11:54:39 -0500

    Bonus applied

--- BankingSolution/Banking.UnitTests/GoldCustomersGetBonusOnDeposit.cs
+++ BankingSolution/Banking.UnitTests/GoldCustomersGetBonusOnDeposit.cs
@@ -1,12 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Banking.UnitTests;
 
-namespace Banking.UnitTests
+public class GoldCustomersGetBonusOnDeposit
 {
-    internal class GoldCustomersGetBonusOnDeposit
+    [Fact]
+    public void BonusAppliedToDeposit() 
     {
+        var account = new BankAccount();
+        var openingBalance = account.GetBalance();
+        var amountToDeposit = 100M;
+
+        account.Deposit(amountToDeposit); 
+
+        Assert.Equal(openingBalance + amountToDeposit + 10M, account.GetBalance());
     }
 }
