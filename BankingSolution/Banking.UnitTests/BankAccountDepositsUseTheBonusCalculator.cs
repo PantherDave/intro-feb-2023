271a74fa08f0a5cdba13261ae72a33fe1a9ee1ff
David Acosta <dacostacsit@gmail.com>
2023-02-09 14:30:49 -0500

    Different class for the interface

---
+++ BankingSolution/Banking.UnitTests/BankAccountDepositsUseTheBonusCalculator.cs
@@ -0,0 +1,17 @@
+﻿namespace Banking.UnitTests;
+
+public class BankAccountDepositsUseTheBonusCalculator
+{
+    [Fact]
+    public void BonusAppliedToDeposit()
+    {
+        // Given
+        var account = new BankAccount(new StubbedBonusCalculator());
+        var openingBalance = account.GetBalance();
+        var amountToDeposit = 118.32M;
+        // When
+        account.Deposit(amountToDeposit); // <- THIS IS THE SYSTEM UNDER TEST
+        // Then
+        Assert.Equal(openingBalance + amountToDeposit + 42.18M, account.GetBalance());
+    }
+}
--- BankingSolution/Banking.UnitTests/NewAccounts.cs
+++ BankingSolution/Banking.UnitTests/NewAccounts.cs
@@ -7,7 +7,7 @@ public class NewAccounts
     [Fact]
     public void NewAccoutnsHaveTheCorrewctOpeningBalance()
     {
-        var account = new BankAccount(new DummyBonusCalculator());
+        var account = new BankAccount(new Mock<ICanCalculateAccountBonuses>().Object);
 
         decimal openingBalance = account.GetBalance();
 
